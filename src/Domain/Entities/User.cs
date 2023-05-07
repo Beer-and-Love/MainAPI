134c17c79f85442a77cc29944f0080f0ae583d61
ArielLopes888 <ariellopes888@gmail.com>
2023-05-07 20:14:10 -0300

    Criando base e entidade de usuário

---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Validators;
+using Domain.Exceptions;
+using Domain.Enuns;
+
+namespace Domain.Entities
+{
+    public class User : Base
+    {
+        public string Name { get;  set; }
+        public string Email { get;  set; }
+        public string Password { get;  set; }
+        public Status Status {get; set;}
+
+        public User() { }
+
+        public User(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+            _errors = new List<string>();
+
+            Validate();
+        }
+
+        public override bool Validate()
+        {
+            var validator = new UserValidator();
+            var validation = validator.Validate(this);
+
+            if (!validation.IsValid)
+            {
+                foreach(var error in validation.Errors)
+                    _errors.Add(error.ErrorMessage);
+                throw new DomainException("Alguns campos estão inválidos, por favor corrigir" , _errors);
+            }
+            return true;
+        }
+    }
+}
