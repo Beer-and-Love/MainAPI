7d5b6801d269c4383f552fcccc8893c0f79b28d5
ArielLopes888 <ariellopes888@gmail.com>
2023-05-08 01:05:10 -0300

    add context

---
+++ src/Infra/Context/ApiContext.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Infra.Mappings;
+
+namespace Infra.Context
+{
+    public class ApiContext:DbContext
+    {
+        public ApiContext()
+        {}
+
+        public ApiContext(DbContextOptions<ApiContext>options) : base(options)
+        {}
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new UserMap());
+        }
+
+    }
+}
