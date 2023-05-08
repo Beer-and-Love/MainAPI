6ab1f661a31744a0716469dd2758ad07196b5246
ArielLopes888 <ariellopes888@gmail.com>
2023-05-08 01:08:30 -0300

    views model

---
+++ src/API/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "O Login não pode ser vazio")]
+        public string Login { get; set; }
+
+
+        [Required(ErrorMessage = "A Senha não pode ser vazio")]
+        public string Password { get; set; }
+    }
+}
