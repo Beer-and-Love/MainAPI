e12a951b11810f6c02bf218c4426c07856530ef8
ArielLopes888 <ariellopes888@gmail.com>
2023-05-10 17:49:41 -0300

    viewmodels

--- src/API/ViewModels/CreateUserViewModel.cs
+++ src/API/ViewModels/CreateUserViewModel.cs
@@ -2,11 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace API.ViewModels
 {
-    public class CreateUserViewModel
+      public class CreateUserViewModel
     {
-        
+        [Required(ErrorMessage = "O nome não pode ser vazio")]
+        [MinLength(3, ErrorMessage = "O nome deve ter no mínimo 3 caracteres.")]
+        [MaxLength(80, ErrorMessage = "O nome deve ter no máximo 80 caracteres.")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "O Email não pode ser vazio")]
+        [MinLength(10, ErrorMessage = "O Email deve ter no mínimo 10 caracteres.")]
+        [MaxLength(180, ErrorMessage = "O Email deve ter no máximo 180 caracteres.")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "A senha não pode ser vazia")]
+        [MinLength(10, ErrorMessage = "A senha deve ter no mínimo 10 caracteres.")]
+        [MaxLength(80, ErrorMessage = "A senha deve ter no máximo 80 caracteres.")]
+        public string Password { get; set; }
     }
 }
