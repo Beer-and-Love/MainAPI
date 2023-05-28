920dc9cb50e56858abec36b16e06bfd51265cd55
ArielLopes888 <ariellopes888@gmail.com>
2023-05-28 01:40:18 -0300

    adding CalculateDistance method and method with Haversine formula

--- src/Service/Services/LocalizationService.cs
+++ src/Service/Services/LocalizationService.cs
@@ -12,9 +12,7 @@ namespace Service.Services
         private readonly IMapper _mapper;
         private readonly ILocalizationRepository _localizationRepository;
         private readonly IUserRepository _userRepository;
-        private LocalizationDto _currentUserLocalization;
-
-
+        private const double EarthRadiusKm = 6371.0;
         public LocalizationService(IMapper mapper, ILocalizationRepository localizationRepository,
             IUserRepository userRepository)
         {
@@ -29,5 +27,69 @@ namespace Service.Services
             return _mapper.Map<List<LocalizationDto>>(allLocalizations);
         }
 
+        public async Task<LocalizationDto> UpdateUserLocalization(long id, LocalizationDto localizationDto)
+        {
+            var user = await _userRepository.Get(id);
+
+            if (user == null)
+                throw new DomainException("Não existe nenhum usuário cadastrado com esse Id");
+
+            user.Localization = _mapper.Map<Localization>(localizationDto);
+            user.Localization.Validate();
+
+            var userUpdated = await _userRepository.Update(user);
+
+            var localizationUpdated = _mapper.Map<LocalizationDto>(userUpdated.Localization);
+            return localizationUpdated;
+        }
+
+        public async Task<List<(UserDto user, double distance)>> CalculateDistance(string city, long currentUserId)
+        {
+
+            var usersByCity = await _userRepository.SearchByCity(city);
+
+            var currentUser = await _userRepository.Get(currentUserId);
+            var currentUserLocalization = _mapper.Map<LocalizationDto>(currentUser.Localization);
+
+            List<(UserDto user, double distance)> distances = new List<(UserDto user, double distance)>();
+
+
+            foreach (var user in usersByCity)
+            {
+                var userDto = _mapper.Map<UserDto>(user);
+                var userLocalization = _mapper.Map<LocalizationDto>(user.Localization);
+                double distance = CalculateDistanceBetweenLocations(currentUserLocalization, userLocalization);
+
+                distances.Add((userDto, distance));
+            }
+
+            return distances;
+
+        }
+        private double CalculateDistanceBetweenLocations(LocalizationDto location1, LocalizationDto location2)
+        {
+            double latitude1 = DegreeToRadian(location1.Latitude);
+            double longitude1 = DegreeToRadian(location1.Longitude);
+            double latitude2 = DegreeToRadian(location2.Latitude);
+            double longitude2 = DegreeToRadian(location2.Longitude);
+
+            double deltaLatitude = latitude2 - latitude1;
+            double deltaLongitude = longitude2 - longitude1;
+
+            double a = Math.Pow(Math.Sin(deltaLatitude / 2), 2) +
+                       Math.Cos(latitude1) * Math.Cos(latitude2) *
+                       Math.Pow(Math.Sin(deltaLongitude / 2), 2);
+
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+            double distance = EarthRadiusKm * c;
+
+            return distance;
+        }
+
+        private double DegreeToRadian(double degree)
+        {
+            return degree * Math.PI / 180.0;
+        }
     }
 }
